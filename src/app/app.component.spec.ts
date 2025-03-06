1cd12fff9050cc3ba5447c1a188d5143741418c8
Dinda David <h@stud.u-szeged.hu>
2025-03-06 18:23:02 +0100

    add firebase

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Szakdolgozat' title`, () => {
+  it(`should have the 'szakdolgozat' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Szakdolgozat');
+    expect(app.title).toEqual('szakdolgozat');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Szakdolgozat');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, szakdolgozat');
   });
 });
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,10 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "szakdolgozat-8a45b", appId: "1:741759249396:web:d2d0ffe68f864faa4167a1", databaseURL: "https://szakdolgozat-8a45b-default-rtdb.europe-west1.firebasedatabase.app", storageBucket: "szakdolgozat-8a45b.firebasestorage.app", apiKey: "AIzaSyB1OVKnlwX-Sw9_HNF0BoN4_Ibq36JKVOg", authDomain: "szakdolgozat-8a45b.firebaseapp.com", messagingSenderId: "741759249396", measurementId: "G-80SLD22QNR" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())]
 };
